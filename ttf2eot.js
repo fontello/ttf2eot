ffa33eebcc9cbf9db6778ef886e22939f084f299
Alex Kocharin <alex@kocharin.ru>
2021-07-20 19:27:18 +0300

    Update argparse to v2

--- ttf2eot.js
+++ ttf2eot.js
@@ -14,40 +14,46 @@ var ttf2eot = require('./index.js');
 
 
 var parser = new ArgumentParser ({
-  version: require('./package.json').version,
-  addHelp: true,
+  add_help: true,
   description: 'TTF to EOT font converter'
 });
 
-parser.addArgument (
-  [ 'infile' ],
+parser.add_argument (
+  'infile',
   {
     nargs: '?',
     help: 'Input file (stdin if not defined)'
   }
 );
 
-parser.addArgument (
-  [ 'outfile' ],
+parser.add_argument (
+  'outfile',
   {
     nargs: '?',
     help: 'Output file (stdout if not defined)'
   }
 );
 
+parser.add_argument (
+  '-v', '--version',
+  {
+    action: 'version',
+    version: require('./package.json').version,
+    help: "show program's version number and exit"
+  }
+);
+
 /* eslint-disable no-console */
 
-var args = parser.parseArgs();
+var args = parser.parse_args();
 
-var input, size;
+var input;
 
 try {
   if (args.infile) {
     input = fs.readFileSync(args.infile);
   } else {
-    size = fs.fstatSync(process.stdin.fd).size;
-    input = new Buffer(size);
-    fs.readSync(process.stdin.fd, input, 0, size, 0);
+    input = fs.readFileSync(process.stdin.fd);
   }
 } catch (e) {
   console.error("Can't open input file (%s)", args.infile || 'stdin');
