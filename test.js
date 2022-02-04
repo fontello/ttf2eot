410b990d339cd86b7ab2537aa08dc07980205b15
Sonia Seddiki <sseddiki@pitchy.fr>
2022-02-04 18:13:49 +0100

    Add validation test

--- test.js
+++ test.js
@@ -6,6 +6,12 @@ var assert  = require('assert');
 var fs      = require('fs');
 var ttf2eot = require('.');
 
+it('bin compare - OS/2 version 0', function () {
+  var src = new Uint8Array(fs.readFileSync('./fixtures/test_v0.ttf'));
+  var dst = new Uint8Array(fs.readFileSync('./fixtures/test_v0.eot'));
+
+  assert.deepEqual(ttf2eot(src), dst);
+});
 
 it('bin compare', function () {
   var src = new Uint8Array(fs.readFileSync('./fixtures/test.ttf'));
